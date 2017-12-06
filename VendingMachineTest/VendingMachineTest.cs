82f602f0ad2fa75b26d8a8d149a33b9ee26828a6
Joshua Theisen <dracon9804@gmail.com>
2017-12-06 14:48:30 -0500

    Finished Accept Coin Requirements

--- VendingMachineTest/VendingMachineTest.cs
+++ VendingMachineTest/VendingMachineTest.cs
@@ -22,6 +22,35 @@ namespace ViewModel
 			var initCredit = VendingMachine.CreditValue;
 			VendingMachine.InsertCoin("Quarter");
 			Assert.AreNotEqual(initCredit, VendingMachine.CreditValue);
-		}
+
+            initCredit = VendingMachine.CreditValue;
+            VendingMachine.InsertCoin("Dime");
+            Assert.AreNotEqual(initCredit, VendingMachine.CreditValue);
+
+            initCredit = VendingMachine.CreditValue;
+            VendingMachine.InsertCoin("Nickle");
+            Assert.AreNotEqual(initCredit, VendingMachine.CreditValue);
+        }
+
+        [Test]
+        public void InsertInValidCoinTest()
+        {
+            var initCredit = VendingMachine.CreditValue;
+            VendingMachine.InsertCoin("Penny");
+            Assert.AreEqual(initCredit, VendingMachine.CreditValue);
+        }
+
+        [Test]
+        public void DisplayINSERTCOINWithNoCoinsInserted()
+        {
+            Assert.AreEqual("INSERT COIN", VendingMachine.DisplayString);
+        }
+
+        [Test]
+        public void CoinReturnHasRejectedCoins()
+        {
+            VendingMachine.InsertCoin("Penny");
+            Assert.IsTrue(VendingMachine.CoinReturn.Contains("Penny"));
+        }
     }
 }
